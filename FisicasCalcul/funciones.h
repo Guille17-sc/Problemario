45119121ef334e8773f4be35bd84eb0014920409
ASUS MASTER <ASUS MASTER@DESKTOP-JQ17VS5>
2021-01-19 13:17:05 -0600

    18/12/202O Trabajo en Clase

---
+++ FisicasCalcul/funciones.h
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   funciones.h
+ * Author: ASUS MASTER
+ *
+ * Created on 19 de enero de 2021, 01:12 PM
+ */
+
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+#include <stdio.h>
+
+//Declaraciones Funciones 
+int suma(int a, int b);
+int resta(int a, int b);
+char primer_caracter(char* cadena);
+double are_circulo(int radio);
+
+
+
+#endif /* FUNCIONES_H */
+
