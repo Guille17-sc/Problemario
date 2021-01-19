025551a13371018a3e007a06754095dc56d533d1
ASUS MASTER <ASUS MASTER@DESKTOP-JQ17VS5>
2021-01-19 13:13:28 -0600

    18/12/202O Trabajo en Clase

---
+++ FisicasCalcul/ArchCabe.c
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   ArchCabe.c
+ * Author: Guille Saldaña 
+ *
+ * Created on 17 de enero de 2021, 06:53 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "funciones.h"
+
+int main(){
+
+   int a=3,b=2, c;
+   float area;
+   char inicial;
+
+ c=suma(a,b)
+printf("c: %d\n",c);
+c=resta(a,b);
+printf("c: %d\n",c);
+
+area=area_circulo(2);
+printf("area: %f\n",area);
+
+inicial=primer_caracter("chelin");
+printf("primer caracter: ");
+
+
+return 0;
+
+}
