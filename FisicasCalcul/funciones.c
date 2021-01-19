06845908a92ea6bad2de311007a2e38d204da495
ASUS MASTER <ASUS MASTER@DESKTOP-JQ17VS5>
2021-01-19 13:15:13 -0600

    18/12/202O Trabajo en Clase

---
+++ FisicasCalcul/funciones.c
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//funciones
+#include "funciones.h"
+#include "funciones.h"
+
+float PI=3.1452
+
+int suma (int a, int b){
+ return a+b;
+}
+int resta (int a, int b){
+ return a-b;
+}
+
+char primer_caracter(char* cadena){
+ return cadena[0];
+}
+
+double area_circulo(int radio){
+return PI*circulo*radio;
+
+}
